14a2e4b83869d823337eed4af1f57068bf9e92e1
ryoTt2 <92706094+ryoTt2@users.noreply.github.com>
2021-10-19 11:11:32 +0900

    1st release version

--- main.ts
+++ main.ts
@@ -1,47 +1,14 @@
-makerbit.onIrButton(IrButton.Number_1, IrButtonAction.Pressed, function () {
-	
-})
 input.onButtonPressed(Button.A, function () {
     basic.showIcon(IconNames.Heart)
 })
 makerbit.onIrButton(IrButton.Any, IrButtonAction.Released, function () {
-    if (makerbit.irButton() == DOWN) {
-        basic.showLeds(`
-            . . . . .
-            # # # # #
-            . # # # .
-            . . # . .
-            . . . . .
-            `)
-    } else if (makerbit.irButton() == B0) {
-        basic.showNumber(0)
-    } else if (makerbit.irButton() == CLOCK) {
-        basic.showLeds(`
-            . # # . #
-            # . . # #
-            # . # # #
-            # . . . .
-            . # # . .
-            `)
-    } else if (makerbit.irButton() == MINUS) {
-        basic.showLeds(`
-            . . . . .
-            . . . . .
-            # # # # #
-            . . . . .
-            . . . . .
-            `)
-    } else {
-    	
-    }
-})
-makerbit.onIrButton(IrButton.Up, IrButtonAction.Pressed, function () {
+    SuperBit.MotorStopAll()
     basic.showLeds(`
-        . . . . .
-        . . # . .
-        . # # # .
-        # # # # #
-        . . . . .
+        # . . . .
+        . . # . #
+        # . # # .
+        # . # . .
+        # . # . .
         `)
 })
 makerbit.onIrButton(IrButton.Any, IrButtonAction.Pressed, function () {
@@ -53,14 +20,34 @@ makerbit.onIrButton(IrButton.Any, IrButtonAction.Pressed, function () {
             # # # # #
             . . . . .
             `)
-    } else if (makerbit.irButton() == SPEAKER) {
+        SuperBit.MotorRun(SuperBit.enMotors.M1, 99)
+    } else if (makerbit.irButton() == DOWN) {
         basic.showLeds(`
-            . # . # .
-            # # . . #
-            # # # . #
-            # # . . #
-            . # . # .
+            . . . . .
+            # # # # #
+            . # # # .
+            . . # . .
+            . . . . .
             `)
+        SuperBit.MotorRun(SuperBit.enMotors.M1, -99)
+    } else if (makerbit.irButton() == LEFT) {
+        basic.showLeds(`
+            . . . # .
+            . . # # .
+            . # # # .
+            . . # # .
+            . . . # .
+            `)
+        SuperBit.MotorRun(SuperBit.enMotors.M2, 99)
+    } else if (makerbit.irButton() == RIGHT) {
+        basic.showLeds(`
+            . # . . .
+            . # # . .
+            . # # # .
+            . # # . .
+            . # . . .
+            `)
+        SuperBit.MotorRun(SuperBit.enMotors.M2, -99)
     } else if (makerbit.irButton() == ANTICLOCK) {
         basic.showLeds(`
             # . # # .
@@ -69,14 +56,22 @@ makerbit.onIrButton(IrButton.Any, IrButtonAction.Pressed, function () {
             . . . . #
             . . # # .
             `)
-    } else if (makerbit.irButton() == PLUS) {
+        SuperBit.MotorRun(SuperBit.enMotors.M3, 99)
+    } else if (makerbit.irButton() == CLOCK) {
         basic.showLeds(`
-            . . # . .
-            . . # . .
-            # # # # #
-            . . # . .
-            . . # . #
+            . # # # .
+            # . . . #
+            # . # . #
+            . . # # .
+            . . # # #
             `)
+        SuperBit.MotorRun(SuperBit.enMotors.M3, -99)
+    } else if (makerbit.irButton() == PLUS) {
+        basic.showString("+")
+        SuperBit.MotorRun(SuperBit.enMotors.M4, 99)
+    } else if (makerbit.irButton() == MINUS) {
+        basic.showString("-")
+        SuperBit.MotorRun(SuperBit.enMotors.M4, -99)
     } else {
     	
     }
@@ -84,35 +79,26 @@ makerbit.onIrButton(IrButton.Any, IrButtonAction.Pressed, function () {
 input.onButtonPressed(Button.B, function () {
     music.playTone(262, music.beat(BeatFraction.Whole))
 })
-makerbit.onIrButton(IrButton.Right, IrButtonAction.Pressed, function () {
-    basic.showLeds(`
-        . # . . .
-        . # # . .
-        . # # # .
-        . # # . .
-        . # . . .
-        `)
-})
 let MINUS = 0
-let B0 = 0
 let PLUS = 0
 let CLOCK = 0
 let DOWN = 0
 let ANTICLOCK = 0
-let SPEAKER = 0
+let RIGHT = 0
+let LEFT = 0
 let UP = 0
 makerbit.connectIrReceiver(DigitalPin.P1, IrProtocol.Keyestudio)
 let POWER = 0
 UP = 128
 let BULB = 64
-let LEFT = 32
-SPEAKER = 160
-let RIGHT = 96
+LEFT = 32
+let SPEAKER = 160
+RIGHT = 96
 ANTICLOCK = 16
 DOWN = 144
 CLOCK = 80
 PLUS = 48
-B0 = 176
+let B0 = 176
 MINUS = 112
 let B1 = 8
 let B2 = 136
@@ -123,3 +109,10 @@ let B6 = 104
 let B7 = 24
 let B8 = 152
 let B9 = 88
+basic.showLeds(`
+    # . . . .
+    . . # . #
+    # . # # .
+    # . # . .
+    # . # . .
+    `)
